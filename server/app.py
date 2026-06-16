c97de0c0fdff518e889c4180e3c8d2f07be5ddc3
root <root@DESKTOP-F7O95GB.localdomain>
2026-06-16 23:24:35 +0300

    changed the app.py file

--- server/app.py
+++ server/app.py
@@ -1,17 +1,14 @@
 import os
 from flask import Flask, send_from_directory
 
-# Point Flask to your React build folder
 app = Flask(__name__, static_folder="../qr-validator/dist", static_url_path="")
 
 # Catch-all route: serve index.html for any path
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def serve(path):
-    file_path = os.path.join(app.static_folder, path)
-
     # If the requested file exists (like JS/CSS), serve it
-    if os.path.isfile(file_path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
 
     # Otherwise, always serve index.html
