caba5cf7df9336674f9db66259bec4f246ebd6ff
root <root@DESKTOP-F7O95GB.localdomain>
2026-06-16 21:03:08 +0300

    Added static.json file and building a new deployment folder

--- qr-validator/src/pages/QrGenerator.jsx
+++ qr-validator/src/pages/QrGenerator.jsx
@@ -2,7 +2,7 @@ import QRCode from "react-qr-code";
 
 function QrGenerator() {
   const url =
-    "https://stirring-scone-88d88b.netlify.app/validate/8DEB11A545EE76E";
+    "https://epay-kwale.onrender.com/validate/8DEB11A545EE76E";
 
   return (
     <div className="flex justify-center items-center min-h-screen">
