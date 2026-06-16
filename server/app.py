abe6a8fed29f70a2401f2fcac2ccf818abd9b74c
root <root@DESKTOP-F7O95GB.localdomain>
2026-06-16 23:34:34 +0300

    removed static.json file

--- server/app.py
+++ server/app.py
@@ -7,12 +7,10 @@ app = Flask(__name__, static_folder="../qr-validator/dist", static_url_path="")
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def serve(path):
-    # If the requested file exists (like JS/CSS), serve it
-    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+    if path and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
-
-    # Otherwise, always serve index.html
     return send_from_directory(app.static_folder, "index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
